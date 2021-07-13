c206313f4cce031db6f56ab3d7000425a2014bf8
NeeharSheth <75029741+NeeharSheth@users.noreply.github.com>
2021-07-13 14:58:06 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
+console.log("mu5 version:",ml5.version);
+
 function setup() {
-  canvas = createCanvas(300, 300);
+  canvas = createCanvas(250, 220);
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classfier= ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function modelLoaded(){
+  console.log('Model Loaded :D');
 }
 
 function draw(){
-image(video,0,0,300,300);
+image(video,0,0,250,220);
+classfier.classify(video, gotResult);
+}
+
+function gotResult(error, result){
+  if(error){
+console.log(error)
+  }
+  else{
+console.log(result);
+document.getElementById("object_result").innerHTML= result[0].label;
+document.getElementById("accuracy_result").innerHTML= result[0].confidence.toFixed(2)*100+"%";
+  }
 }
 
 
